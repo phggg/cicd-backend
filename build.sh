1794caeee9927d855914e6c205d3ba024f75b383
zhaoziwei <1728434772@qq.com>
2022-07-16 14:35:02 +0800

    fix: 修改端口

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #bin/bash
 time=$(date "+%Y%m%d%H%M%S")
-docker build -t 81.70.173.234/:8082/backend-app:$time .
-docker push 81.70.173.234/:8082/backend-app:$time
+docker build -t 81.70.173.234:8082/backend-app:$time .
+docker login -u admin -p 19961126 81.70.173.234:8082
+docker push 81.70.173.234:8082/backend-app:$time
